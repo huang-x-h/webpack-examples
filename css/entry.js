3896c2adcf9040c85c078a184fdad7bfe4cf2657
huang.xinghui <huang.x.hui@gmail.com>
2016-07-01 16:47:24 +0800

    add css example

---
+++ css/entry.js
@@ -0,0 +1,4 @@
+require('./post/index.css')
+require('./about/index.css')
+
+console.log('I am a example.')
---
+++ css/webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  context: __dirname,
+  entry: './entry.js',
+  output: {
+    path: __dirname,
+    filename: "entry.bundle.js",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      },
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('main.css')
+  ]
+}
